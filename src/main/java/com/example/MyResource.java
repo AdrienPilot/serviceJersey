e4fbad4e41bcd20eb800c7c6870c956dd48c059b
Adrien Pilot <adrien.pilot@etudiant.univ-lr.fr>
2022-11-22 11:18:36 +0100

    [ADD] Ended TP

--- src/main/java/com/example/MyResource.java
+++ src/main/java/com/example/MyResource.java
@@ -2,14 +2,22 @@ package com.example;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Root resource (exposed at "myresource" path)
  */
 @Path("myresource")
 public class MyResource {
+    static Map<String, Integer> nameOccurence = new HashMap<>();
+    @Context
+    UriInfo uriInfo;
 
     /**
      * Method handling HTTP GET requests. The returned object will be sent
@@ -18,8 +26,19 @@ public class MyResource {
      * @return String that will be returned as a text/plain response.
      */
     @GET
+    @Path("{name}")
     @Produces(MediaType.TEXT_PLAIN)
-    public String getIt() {
-        return "Got it!";
+    public String getIt(@PathParam("name") String name) {
+        if (name == null) {
+            return "Aucun paramètre";
+        }
+        if (nameOccurence.containsKey(name)) {
+            nameOccurence.put(name, nameOccurence.get(name) + 1);
+            String str =  name + " " + nameOccurence.get(name) + " occurence(s)";
+            return str;
+        } else {
+            nameOccurence.put(name, 1);
+            return name;
+        }
     }
 }
